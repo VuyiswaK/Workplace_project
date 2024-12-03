2d0090ecc9eaec6cbd5fb54a50026dcfd5e2c351
VuyiswaK <vuyiswakubalasa01@gmail.com>
2024-12-04 00:43:14 +0200

    final model

--- Streamlit/base_app.py
+++ Streamlit/base_app.py
@@ -18,9 +18,9 @@ as part of your predict project.
 For further help with the Streamlit framework, see:
 
 https://docs.streamlit.io/en/latest/
+
 """
 
-# Streamlit dependencies
 import streamlit as st
 import joblib
 import os
@@ -29,24 +29,20 @@ import re
 import requests
 from io import BytesIO  # Import BytesIO
 import numpy as np
+import requests
+import base64
+import pickle
 
 # Function to fetch and load a pickle file from a URL
-def load_pickle_from_url(url):
-    response = requests.get(url)
+def load_pickle_from_url(url, token):
+    headers = {'Authorization': f'token {token}'}
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
         # Use BytesIO to treat the bytes response as a file-like object
         return joblib.load(BytesIO(response.content))
     else:
         st.error(f"Error loading the file from URL: {url} (Status code: {response.status_code})")
         return None
-
-# Vectorizer URL
-#vectorizer_url = "https://raw.githubusercontent.com/VuyiswaK/2401PTDS_Classification_Project/main/Streamlit/tfidfvect.pkl"
-#test_cv = load_pickle_from_url(vectorizer_url)
-
-# Load your raw data
-#raw = pd.read_csv('https://raw.githubusercontent.com/VuyiswaK/2401PTDS_Classification_Project/main/Streamlit/train.csv')
-
 
 # The main function where we will build the actual app
 def main():
@@ -70,7 +66,6 @@ def main():
 
     if selection == "Prediction":
         st.info("Prediction with Random Forest Model")
-        #st.image("https://raw.githubusercontent.com/VuyiswaK/2401PTDS_Classification_Project/main/Streamlit/science-in-the-news.png", use_column_width=True)
         
         # Creating a text box for user input
         Salary = st.number_input("Salary:", min_value=0, max_value=10000000, value=0)
@@ -97,15 +92,7 @@ def main():
         option6 = np.array([*range(1,101)])/100
         Spend_percentage = st.selectbox("Spend percentage:", option6)
 
-        #news_text = st.text_area("Enter news headline, content or description here", "Type Here")
-
         if st.button("Predict"):
-            # Clean input text
-            #cleaned_text = clean(news_text)
-            
-            # Transforming user input with vectorizer
-            #vect_text = test_cv.transform([cleaned_text]).toarray()
-
             variables = np.array([Salary,
                                   Internal_PD,
                                   External_PD,
@@ -115,17 +102,17 @@ def main():
                                   External_utilisation,
                                   Spend_percentage])
 
-            # Load your model from the URL
-            model_url = "https://raw.githubusercontent.com/VuyiswaK/2401PTDS_Classification_Project/main/Streamlit/prediction_model.pkl"
-            predictor = load_pickle_from_url(model_url)
+            # Replace with your GitHub personal access token
+            token = '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'
+            # Use the raw URL for the pickle file
+            model_url = 'https://raw.githubusercontent.com/VuyiswaK/Workplace_project/main/Streamlit/prediction_model.pkl'
+
+            model = load_pickle_from_url(model_url, token)
             
-            if predictor is not None:
-                prediction = predictor.predict(variables)
-                st.success("Text Category: {}".format(prediction))
+            if model is not None:
+                prediction = model.predict([variables])
+                st.success(f"Text Category: {prediction}")
 
 # Required to let Streamlit instantiate our web app.  
 if __name__ == '__main__':
     main()
-
-
- 
